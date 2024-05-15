f05fdcc73af15f70a59bf326691ca2b1165d0d74
Mohit <168701475+Mohitsharma900@users.noreply.github.com>
2024-05-15 15:15:15 +0000

    Conversion-Operations...

---
+++ 01_Basics/03_ConversionOperations.js
@@ -0,0 +1,22 @@
+let score = "33abc";
+
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof(valueInNumber));
+
+console.log(valueInNumber);
+//in case of null u will get 0 on conversion with this way..and in boolean value 0 & 1 
+//and in undefined u will get nan;
+// "33" => 33;
+// "33abc" => NaN;
+// true => 1;
+// NUll => 0;
+ 
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+
+// While conversion You should always cross check the ans..
+
