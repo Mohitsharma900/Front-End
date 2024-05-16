ac9341f9b539431a6712dc6da964bd9c4cfbc662
Mohit <168701475+Mohitsharma900@users.noreply.github.com>
2024-05-16 07:12:34 +0000

    Stack and Heap Memories..

--- 01_Basics/dataTypes-Summary.js
+++ 01_Basics/dataTypes-Summary.js
@@ -18,6 +18,8 @@ console.log(id === anotherId);
 
 
 
+
+
 // Reference (Non primitive)
 
 // Array, Objects, Functions
@@ -35,3 +37,37 @@ const myFunction = function(){
 console.log(typeof myFunction);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+//***************************************************************** */
+
+// Stack (Primitive).. Heap (Non-Primitive)..
+// Stack & Heap are memory types..
+// Stack memory me var declare karne se copy milti hai and heap se refference..
+let myYoutubeName= "MohitSharma"
+let anotherName = myYoutubeName;
+
+anotherName = "Plucky Roasts";
+console.log(anotherName);
+console.log(myYoutubeName)
+
+let userOne = {
+    email : "user@google.com",
+    upi : "user@ybl"
+}
+
+let userTwo = userOne;
+userTwo.email = "mohit@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+/*
+ so basically when you changed the value of stack
+ data it returns the copy and thus not change the value of the equal one
+ but when you do the same with heap it takes the value as reference for
+ both the stated objs and thus change the value of both..
+ */
+
+ 
+// stack is one above another..
+// heap is a one big container.. 
