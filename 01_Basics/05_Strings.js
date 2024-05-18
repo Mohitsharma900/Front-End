5398289e7fbf7ac79419778dab6427781ba725a9
Mohit <168701475+Mohitsharma900@users.noreply.github.com>
2024-05-18 06:29:33 +0000

    commit

--- 01_Basics/05_Strings.js
+++ 01_Basics/05_Strings.js
@@ -36,6 +36,7 @@ console.log(newStringOne);
 console.log(newStringOne.trim());
 
 const url = "https://hitesh.com/hitesh%20choudhary"
+
 console.log(url.replace('%20', '-'))
 
 console.log(url.includes('sundar'))
