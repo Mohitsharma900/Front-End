2deacd618c072d82c93148e780bde488720f718a
Mohit <168701475+Mohitsharma900@users.noreply.github.com>
2024-05-20 09:55:40 +0000

    functions_1

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -1,3 +1,51 @@
 /*function is a package in which you store your
 lines of code which can be executed multiple 
 times when you call it*/
+
+// console.log("M");
+// console.log("O");
+// console.log("H");
+// console.log("I");
+// console.log("T");
+
+// function sayMyName() {
+// console.log("M");
+// console.log("O");
+// console.log("H");
+// console.log("I");
+// console.log("T");
+// }
+
+// sayMyName();
+
+
+/*                       parameters     */                
+// function addTwoNumbers(number1, number2) {
+//     console.log(number1 + number2) 
+// }
+
+/*           arguements */
+// addTwoNumbers(3, 8);
+// addTwoNumbers(3, "8");//38
+
+//return
+function addTwoNumbers(number1, number2) {
+    let result= number1 + number2
+    return(result) ;
+}
+
+const result = addTwoNumbers(2, 10);
+console.log(result)
+
+function loginUserMessage(username = "sam"){
+    if(!username/*(professional way of writing "username === undefined")*/){
+        console.log("PLease enter a username");
+        return
+    }//won't enter this block as sam as default value is already passed
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("hitesh"))
+console.log(loginUserMessage("hitesh")) //this will replace sam
+
+
