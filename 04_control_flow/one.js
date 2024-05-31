9c0a2910dc8362ecb13e37edff172bff6ab0f4cd
Mohit <168701475+Mohitsharma900@users.noreply.github.com>
2024-05-31 10:31:56 +0530

    CHANGES

---
+++ 04_control_flow/one.js
@@ -0,0 +1,20 @@
+// if
+// const isUserLoggedIn = true ; 
+// const temp = 41
+
+// if(temp === 40 ){
+//    console.log("temp is 41 ")
+// }else{
+//     console.log("temp is not 41")
+// }
+
+// // < , >, <=, >=, ==, !=, ===, !==
+
+// const score = 200;
+
+// if(score > 100){
+//     const power= "fly";
+//     console.log(`user power: ${power}`)
+// }
+
+// console.log(`user power: ${power}`) can't be accessed globally if not declared using var so you should avoid var
