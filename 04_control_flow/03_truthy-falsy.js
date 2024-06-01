0c39d41e5d2b0086bed9cb1daa5c7f42225cb20b
Mohit <168701475+Mohitsharma900@users.noreply.github.com>
2024-06-01 12:10:51 +0530

    changes

---
+++ 04_control_flow/03_truthy-falsy.js
@@ -0,0 +1,30 @@
+const userEmail = "mohit909gmail.com"
+
+if (userEmail){
+    console.log("Got user Email")
+}else {
+    console.log("Don't have user Email")
+}
+
+/* 
+
+falsy value:
+false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+truthy values:
+"0", "false", " ", [], {}, function(){},
+
+*/
+
+/* 
+const names = [];
+if (names.length === 0){
+    console.log("Arr is empty.");
+}
+
+const obj = {};
+if(Object.keys(obj).length === 0){
+    console.log("Object is empty.");
+}
+*/
+
