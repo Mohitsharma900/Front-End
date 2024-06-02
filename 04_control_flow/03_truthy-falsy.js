323a3620aaa482edb9b32b7c24dda3e3005b5a21
Mohit <168701475+Mohitsharma900@users.noreply.github.com>
2024-06-02 08:18:04 +0530

    changes

--- 04_control_flow/03_truthy-falsy.js
+++ 04_control_flow/03_truthy-falsy.js
@@ -28,3 +28,20 @@ if(Object.keys(obj).length === 0){
 }
 */
 
+//Nullish Coalescing Operator(??): null undefined:
+
+let val1;
+val1= 5 ?? 10
+val1= null ?? 10
+val1= undefined ?? 15
+console.log(val1);//5
+
+//Terniary Operator
+
+// condition ? true : false;
+
+const iceTeaPrice = 100;
+iceTeaPrice>=80 ? console.log("more than 80") : 
+console.log("less than 80");
+
+
