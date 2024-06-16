d12363ba1886be4ea76f71a355f75805689aa423
Mohit <168701475+Mohitsharma900@users.noreply.github.com>
2024-06-16 11:27:35 +0530

    changs

--- 11_classes_and_oop/mathpi.js
+++ 11_classes_and_oop/mathpi.js
@@ -1,4 +1,23 @@
-Object.getOwnPropertyDescriptors(Math)
+const descripter = Object.getOwnPropertyDescriptors(Math, "PI")
+console.log(descripter);
 
-console.log(Math.PI);
-Math.PI = 5;
+// console.log(Math.PI);
+// Math.PI = 5;
+// console.log(Math.PI);
+
+const chai =  {
+    name : 'ginger chai' , 
+    price : 250,
+    isAvailable : true
+}
+
+console.log(Object.getOwnPropertyDescriptor(chai , "name"));
+
+Object.defineProperty(chai, "name", {
+    writable: false,
+    enumerable: false
+})
+
+chai.name ="Mohit"
+
+console.log(Object.getOwnPropertyDescriptor(chai , "name"));
